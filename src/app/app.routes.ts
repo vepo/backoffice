f9ae0b3148409eb0e5f885d199fdd23c19937d21
Victor Osório <victor.perticarrari@gmail.com>
2026-01-10 09:24:02 -0300

    Adding User CRUD

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,17 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login.component/login.component';
+import { authGuard } from './services/auth.guard';
+import { usersResolver } from './resolvers/users.resolver';
+import { UsersViewComponent } from './components/users-view.component/users-view.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
+    {
+        path: 'users',
+        component: UsersViewComponent,
+        resolve: {
+            users: usersResolver
+        },
+        canActivate: [authGuard],
+    },
 ];
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,78 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface User {
+  id: number;
+  username: string;
+  name: string;
+  email: string;
+  roles: string[];
+}
+
+export interface UserSearchFilter {
+  name: string;
+  email: string;
+  roles: string[];
+}
+
+export interface UpdateOrCreateUserRequest {
+  name: string;
+  username: string;
+  email: string;
+  roles: string[];
+}
+
+export function emptyFilter(): UserSearchFilter {
+  return {
+    name: '',
+    email: '',
+    roles: []
+  };
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private readonly http = inject(HttpClient);
+
+  private readonly API_URL = '/api';
+
+  findById(userId: number): Observable<User> {
+    return this.http.get<User>(`${this.API_URL}/users/${userId}`);
+  }
+
+  create(user: UpdateOrCreateUserRequest): Observable<User> {
+    return this.http.post<User>(`${this.API_URL}/users`, user);
+  }
+
+  update(userId: number, user: UpdateOrCreateUserRequest): Observable<User> {
+    return this.http.post<User>(`${this.API_URL}/users/${userId}`, user);
+  }
+
+  search(filter?: UserSearchFilter): Observable<User[]> {
+    let params = new HttpParams();
+
+    if (filter) {
+      Object.keys(filter).forEach(key => {
+        const value = filter[key as keyof UserSearchFilter];
+
+        // Skip null, undefined, empty string, or empty array
+        if (value !== null && value !== undefined && value !== '' &&
+          !(Array.isArray(value) && value.length === 0)) {
+
+          // Handle array values by joining them or adding multiple params
+          if (Array.isArray(value)) {
+            console.debug("Appending query array", value);
+            value.forEach(item => params = params.append(key, item));
+          } else {
+            params = params.append(key, value.toString());
+          }
+        }
+      });
+    }
+
+    return this.http.get<User[]>(`${this.API_URL}/users/search`, { params: params });
+  }
+}
