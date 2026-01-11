c6bc269e4c9697bfef320ddde36d35ac83267624
Victor Osório <victor.perticarrari@gmail.com>
2026-01-11 19:57:46 -0300

    Implementa CRUD

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login.component/login.component';
 import { authGuard } from './services/auth.guard';
-import { usersResolver } from './resolvers/users.resolver';
+import { userResolver, usersResolver } from './resolvers/users.resolver';
 import { UsersViewComponent } from './components/users-view.component/users-view.component';
 import { UsersEditComponent } from './components/users-edit.component/users-edit.component';
 
@@ -18,6 +18,14 @@ export const routes: Routes = [
     {
         path: 'users/new',
         component: UsersEditComponent,
+        canActivate: [authGuard],
+    },
+    {
+        path: 'users/:userId',
+        component: UsersEditComponent,
+        resolve: {
+            user: userResolver
+        },
         canActivate: [authGuard],
     },
 ];
